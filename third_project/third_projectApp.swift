5d26996542e552600c1636c1be3f03cead13994c
GithubDenis5 <gromacihindenis@gmail.com>
2026-03-02 22:09:50 +0300

    add third_project

---
+++ third_project/third_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  third_projectApp.swift
+//  third_project
+//
+//  Created by Денис Громачихин on 20.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct third_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
